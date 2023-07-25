c057403b718ef03e826e0f62cf5e7c0596d43b6f
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-07-25 15:28:03 +0900

    訂正
    ログインアウト時にも一覧表を閲覧できるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!,except: [:show]
-
+  #{}before_action :authenticate_user!,except: [:show]
+  before_action :move_to_index, except: [:show]
   def show
     @user = User.find(params[:id])  # ユーザー情報を取得し、@userに代入
     @occupation_user = @user.occupation
@@ -12,4 +12,10 @@ class UsersController < ApplicationController
   def edit
   end
 
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
